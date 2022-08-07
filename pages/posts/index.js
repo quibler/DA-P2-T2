163baaf6ea0f14e307c1dbf8fde96fd7439f6941
quibler <ashwinidroid@gmail.com>
2022-08-07 12:47:21 +0530

    Section 2 - fetch and display data from api

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,30 +1,42 @@
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(false)
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true)
-  }, [])
+    setLoading(true);
+    getData();
+  }, []);
 
-  if (isLoading) return <p className="text-gray-700 text-base">Fetching Posts...</p>
-  if (!data) return <p>No POSTS fetched</p>
+  const getData = async () => {
+    const api = "https://jsonplaceholder.typicode.com/posts";
+    const res = await fetch(api);
+    const posts = await res.json();
+    setData(posts);
+    setLoading(false);
+  };
+
+  if (isLoading)
+    return <p className="text-gray-700 text-base">Fetching Posts...</p>;
+  if (!data) return <p>No POSTS fetched</p>;
 
   return (
     <div>
       <h1 className="font-bold text-2xl m-2">POSTS</h1>
-      {data.map(post =>
-        <div className="m-2 rounded overflow-hidden shadow-lg">
+      {data.map((post) => (
+        <div key={post.id} className="m-2 rounded overflow-hidden shadow-lg">
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">{post.title}</div>
             <p className="text-gray-700 text-base"> {post.body} </p>
           </div>
           <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">#{post.id}</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">
+              #{post.id}
+            </span>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
+  );
 }
